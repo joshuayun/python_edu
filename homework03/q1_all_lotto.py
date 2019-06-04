cf4307faf32049a2c86383e1a26d1bd0b0463238
myungwoo.yun <myungwoo.yun@playd.com>
2019-06-04 12:14:28 +0900

    숙제 3번쨰 완료

--- homework03/q1_all_lotto.py
+++ homework03/q1_all_lotto.py
@@ -2,15 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
+
+
 wb = Workbook()
+
+# 현재 Active Sheet 얻기
 ws = wb.active
 
-last_lotto_num = 861
+ws.cell(1, 1, value='회차')
+ws.cell(1, 2, value='번호1')
+ws.cell(1, 3, value='번호2')
+ws.cell(1, 4, value='번호3')
+ws.cell(1, 5, value='번호4')
+ws.cell(1, 6, value='번호5')
+ws.cell(1, 7, value='번호6')
+ws.cell(1, 8, value='보너스 번호')
 
-for row in range(last_lotto_num):
-    url = "https://search.daum.net/search?w=tot&rtmaxcoll=LOT&DA=LOT&q="+str(row)+"%ED%9A%8C%EC%B0%A8%20%EB%A1%9C%EB%98%90"
-    print(url)
-    req = requests.get(url)
+
+last_lotto_url = "https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=%EB%A1%9C%EB%98%90"
+last_lotto_req = requests.get(last_lotto_url)
+
+if last_lotto_req.status_code != requests.codes.ok:
+    exit()
+
+last_lotto_html = BeautifulSoup(last_lotto_req.text, 'html.parser')
+last_lotto_area = last_lotto_html.select_one('.tit_lotto > .f_red')
+last_lotto_num = last_lotto_area.text[:-1]
+last_lotto_num = int(last_lotto_num)
+
+print("최종 로또회차:",last_lotto_num)
+
+for lotto_count in range(1, last_lotto_num+1):
+    print(lotto_count,"회차 수집")
+    lotto_url = "https://search.daum.net/search?w=tot&rtmaxcoll=LOT&DA=LOT&q="+str(lotto_count)+"%ED%9A%8C%EC%B0%A8%20%EB%A1%9C%EB%98%90"
+    req = requests.get(lotto_url)
 
     if req.status_code != requests.codes.ok:
         exit()
@@ -18,8 +43,12 @@ for row in range(last_lotto_num):
     html = BeautifulSoup(req.text, 'html.parser')
     lotto_numbers = html.select('span.img_lotto')
 
-    for index, number in enumerate(lotto_numbers, start=1):
-        ws.cell(row, index, value=int(number.text))
-    row += 1
 
-wb.save("lotto_all.xlsx")
+    row_count = lotto_count + 1;
+    ws.cell(row_count, 1, value=str(lotto_count) + "회차")
+    for index, number in enumerate(lotto_numbers, start=2):
+        ws.cell(row_count, index, value=int(number.text))
+
+
+wb.save("lotto_list.xlsx")
+wb.close()
